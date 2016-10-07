5326eb1de36ecdd0f070903c63d206e576488232
Charles Martin <cpm@charlesmartin.com.au>
2016-10-07 12:04:26 +0200

    Initial Commit

---
+++ chirpey/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  chirpey
+//
+//  Created by Charles Martin on 7/10/16.
+//  Copyright © 2016 Charles Martin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ chirpey/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  chirpey
+//
+//  Created by Charles Martin on 7/10/16.
+//  Copyright © 2016 Charles Martin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
