69d0358e4008cf899af4b7ffbe4d4f7229401212
Charles Martin <cpm@charlesmartin.com.au>
2016-12-14 10:39:56 +0100

    finally working with new pods

--- chirpey/chirpey-Bridging-Header.h
+++ chirpey/chirpey-Bridging-Header.h
@@ -2,10 +2,7 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-
+#import "InAppSettingsKit/IASKAppSettingsViewController.h"
 #import "PdAudioController.h"
 #import "PdBase.h"
 #import "PdFile.h"
-#import "ChirpView.h"
-#import "InAppSettingsKit/IASKAppSettingsViewController.h"
-
