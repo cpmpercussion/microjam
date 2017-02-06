a61fac7f66dee4af96e031c154fbba8abf40536d
Charles Martin <cpm@charlesmartin.com.au>
2017-02-06 13:25:06 +0100

    updated pods

--- Pods/Target Support Files/Pods-microjam/Pods-microjam-frameworks.sh
+++ Pods/Target Support Files/Pods-microjam/Pods-microjam-frameworks.sh
@@ -90,10 +90,12 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/InAppSettingsKit/InAppSettingsKit.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/UIColor_Hex_Swift/UIColor_Hex_Swift.framework"
   install_framework "$BUILT_PRODUCTS_DIR/libpd/libpd.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/InAppSettingsKit/InAppSettingsKit.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/UIColor_Hex_Swift/UIColor_Hex_Swift.framework"
   install_framework "$BUILT_PRODUCTS_DIR/libpd/libpd.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
