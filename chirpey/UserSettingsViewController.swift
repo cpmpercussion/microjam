b6e466dd14978da82fd6a046a4e8323239239f5d
Charles Martin <cpm@charlesmartin.com.au>
2017-08-02 13:50:28 +1000

    added in view controller and storyboard for user settings

---
+++ chirpey/UserSettingsViewController.swift
@@ -0,0 +1,43 @@
+//
+//  UserSettingsViewController.swift
+//  microjam
+// 
+//  Manages User Settings such as ID, performer name and avatar
+//  That 
+//  Created by Charles Martin on 2/8/17.
+//  Copyright © 2017 Charles Martin. All rights reserved.
+//
+
+import UIKit
+import CloudKit
+
+class UserSettingsViewController: UIViewController {
+    
+    static func storyboardInstance() -> UserSettingsViewController? {
+        let storyboard = UIStoryboard(name:"UserSettingsViewController", bundle: nil)
+        return storyboard.instantiateInitialViewController() as? UserSettingsViewController
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
