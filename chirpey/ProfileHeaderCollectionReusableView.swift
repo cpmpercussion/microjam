3b84d5bfa07c3e88280667d9181387097284b67e
Charles Martin <cpm@charlesmartin.com.au>
2018-07-12 12:20:19 +1000

    updated new profile screen to work just like the old one :-)

--- chirpey/ProfileHeaderCollectionReusableView.swift
+++ chirpey/ProfileHeaderCollectionReusableView.swift
@@ -7,7 +7,84 @@
 //
 
 import UIKit
+import DropDown
 
 class ProfileHeaderCollectionReusableView: UICollectionReusableView {
+    
+    /// Stack for the whole profile screen
+    @IBOutlet weak var containerStack: UIStackView!
+    /// stack for the Avatar view and stagename field
+    @IBOutlet weak var identityStack: UIStackView!
+    /// stack for the colour selectors and soundscheme dropdown
+    @IBOutlet weak var settingsStack: UIStackView!
+    /// Activity indicator used when loading avatar.
+    @IBOutlet weak var avatarSpinner: UIActivityIndicatorView!
+    /// Container view for avatar image.
+    @IBOutlet weak var avatarContainerView: UIView! {
+        didSet {
+            avatarContainerView.clipsToBounds = true
+            avatarContainerView.layer.cornerRadius = avatarContainerView.bounds.height / 2
+        }
+    }
+    /// Avatar image view.
+    @IBOutlet weak var avatarImageView: UIImageView!
+    /// Text field for the user's stage name
+    @IBOutlet weak var stageNameField: UITextField!
+    /// Slider to control the jam drawing colour
+    @IBOutlet weak var jamColourSlider: UISlider!
+    /// Slider to control the jam background colour
+    @IBOutlet weak var backgroundColourSlider: UISlider!
+    /// Dropdown menu for selecting SoundScheme
+    let soundSchemeDropDown = DropDown()
+    /// Button connected to the soundscheme dropdown menu.
+    @IBOutlet weak var soundSchemeDropDownButton: UIButton!
+    /// Header view used if not logged in - View shown if user is not logged into iCloud.
+    let noAccountHeaderView = NoAccountWarningStackView()
+    
+    /// updates the profile screen's fields according to the present UserProfile data.
+    @objc func updateUI() {
+        // Display appropriate views if user is not logged in.
+        if UserProfile.shared.loggedIn {
+            noAccountHeaderView.isHidden = true
+        } else {
+            noAccountHeaderView.isHidden = false
+        }
+        let profile = UserProfile.shared.profile // Fill in from local user's profile.
+        avatarImageView.image = profile.avatar
+        avatarImageView.contentMode = .scaleAspectFill
+        avatarContainerView.isHidden = false
+        stageNameField.text = profile.stageName
+        jamColourSlider.setValue(PerformerProfile.hueFrom(colour: profile.jamColour), animated: true)
+        jamColourSlider.tintColor = profile.jamColour
+        jamColourSlider.thumbTintColor = profile.jamColour
+        backgroundColourSlider.tintColor = profile.backgroundColour
+        backgroundColourSlider.thumbTintColor = profile.backgroundColour
+        backgroundColourSlider.setValue(PerformerProfile.hueFrom(colour: profile.backgroundColour), animated: true)
+        soundSchemeDropDownButton.setTitle(SoundSchemes.namesForKeys[profile.soundScheme], for: .normal)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
         
+        // Soundscheme Dropdown initialisation.
+        soundSchemeDropDown.anchorView = soundSchemeDropDownButton // anchor dropdown to intrument button
+        soundSchemeDropDown.dataSource = Array(SoundSchemes.namesForKeys.values) // set dropdown datasource to available SoundSchemes
+        soundSchemeDropDown.direction = .bottom
+        // Action triggered on selection
+        soundSchemeDropDown.selectionAction = {(index: Int, item: String) -> Void in
+            print("ProfileHeader: DropDown selected.", index, item)
+            if let sound = SoundSchemes.keysForNames[item] {
+                UserProfile.shared.profile.soundScheme = Int64(sound)
+                self.updateUI()
+            }
+        }
+        
+        // Set up the no account view and collection view.
+        noAccountHeaderView.frame = CGRect(x: 0, y: 0, width: containerStack.frame.width, height: 100) // TODO: This doesn't work in a stack view derp.
+        containerStack.insertArrangedSubview(noAccountHeaderView, at: 0)
+        
+        // add observer for UserProfile updates.
+        NotificationCenter.default.addObserver(self, selector: #selector(updateUI), name: NSNotification.Name(rawValue: userProfileUpdatedNotificationKey), object: nil)
+    }
+
 }
--- chirpey/ProfilePerfController.swift
+++ chirpey/ProfilePerfController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import CloudKit
 
-/// A subclass of UserPerfController for the special case of the profile screen. Sets up for the local user and removes the header.
+/// View Controller for the profile screen; subclass of UserPerfController to handle the flow layout and fetching of performances from the local user.
 class ProfilePerfController: UserPerfController {
     /// Link to the users' profile data - not used right now, might be used later.
     let profile: PerformerProfile = UserProfile.shared.profile
@@ -22,24 +22,26 @@ class ProfilePerfController: UserPerfController {
     }
     static let headerID = "ProfileSceneHeader"
     static let footerID = "ProfileSceneFooter"
+    /// Storage for the headerView
+    var headerView : ProfileHeaderCollectionReusableView?
 
-    /// Override of viewDidLoad in order to set the perfomerID to the local user.
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        performerID = CKRecordID(recordName: "__defaultOwner__")
-    }
 
+    // MARK: - Collection View Setup
+    
     /// Override of the header function size to be 300
     override func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
-        return CGSize(width: collectionView.frame.width, height: 300)
+        return CGSize(width: collectionView.frame.width, height: 310)
     }
     
     /// Override to present header view from storyboard.
     override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
         switch kind {
         case UICollectionElementKindSectionHeader:
-            let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionElementKindSectionHeader, withReuseIdentifier: ProfilePerfController.headerID, for: indexPath) as! ProfileHeaderCollectionReusableView
-            return headerView
+            headerView = (collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionElementKindSectionHeader, withReuseIdentifier: ProfilePerfController.headerID, for: indexPath) as! ProfileHeaderCollectionReusableView)
+            headerView?.updateUI() // update with latest information
+            headerView?.stageNameField.delegate = self // become delegate for the stagename field.
+            
+            return headerView!
         case UICollectionElementKindSectionFooter:
             let footerView = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionElementKindSectionFooter, withReuseIdentifier: ProfilePerfController.footerID, for: indexPath) as! ProfileFooterCollectionReusableView
             return footerView
@@ -51,7 +53,57 @@ class ProfilePerfController: UserPerfController {
     
     /// Override of the footer function size to be 100 - just needs a few labels.
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
-        return CGSize(width: collectionView.frame.width, height: 100)
+        return CGSize(width: collectionView.frame.width, height: 120)
+    }
+    
+    // MARK: - Life cycle
+    
+    /// Override of viewDidLoad in order to set the perfomerID to the local user.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        performerID = CKRecordID(recordName: "__defaultOwner__")
+        // Set up the header.
+    }
+    
+    /// When the view disappears, updates the profile on iCloud
+    override func viewWillDisappear(_ animated: Bool) {
+        UserProfile.shared.updateUserProfile()
+    }
+    
+    /// When the view will appear, updates UI with latest profile / performance information
+    override func viewWillAppear(_ animated: Bool) {
+        headerView?.updateUI()
+    }
+    
+    // MARK: - Interface builder actions for header
+    
+    /// Triggered when user changes the jam colour slider
+    @IBAction func jamColourSliderMoved(_ sender: UISlider) {
+        let colour = PerformerProfile.colourFromHue(hue: sender.value)
+        sender.tintColor = colour
+        sender.thumbTintColor = colour
+        profile.jamColour = colour
+    }
+    
+    /// Triggered when user changes the background colour
+    @IBAction func backgroundColourSliderMoved(_ sender: UISlider) {
+        let colour = PerformerProfile.colourFromHue(hue: sender.value)
+        sender.tintColor = colour
+        sender.thumbTintColor = colour
+        profile.backgroundColour = colour
+    }
+    
+    /// Opens the SoundScheme dropdown menu.
+    @IBAction func soundSchemeTapped(_ sender: Any) {
+        headerView?.soundSchemeDropDown.show()
+    }
+    
+    /// Open image picker to change avatar.
+    @IBAction func changeAvatar(_ sender: Any) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.delegate = self
+        present(imagePicker, animated: true, completion: nil)
     }
     
     //    MARK: - Interface Builder Actions for footer
@@ -70,4 +122,46 @@ class ProfilePerfController: UserPerfController {
         }
     }
     
+    @IBAction func exportDataButtonTapped(_ sender: Any) {
+        // FIXME: Fill this in.
+    }
+    
+    @IBAction func deleteDataButtonTapped(_ sender: Any) {
+    }
+    
+}
+
+
+/// Stage Name Chooser extension
+/// Adds functions to handle user changing the UITextField for their stage name on the settings screen.
+extension ProfilePerfController: UITextFieldDelegate {
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        return true
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        if let newName = headerView?.stageNameField.text {
+            UserProfile.shared.profile.stageName = newName
+            UserProfile.shared.updateUserProfile()
+            print("ProfileHeader: Set stage name to: ", newName)
+        }
+        return true
+    }
+}
+
+/// Extensions to UserSettingsViewController to interact with an image picker and navigatino controller.
+extension ProfilePerfController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        defer {
+            picker.dismiss(animated: true, completion: nil)
+        }
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            print("ProfileHeader: Updating image")
+            UserProfile.shared.updateAvatar(image)
+            headerView?.avatarImageView.image = image
+        }
+    }
 }
